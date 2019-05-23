5f41ee38e16c027ce2500ab12353be19b87e3515
codewithmmd <codewithmmd@gmail.com>
2019-05-23 11:34:36 +0530

    Test The Paste Event

--- DOM/app.js
+++ DOM/app.js
@@ -6,3 +6,24 @@
 
 // let forms = document.forms[0];
 // console.log(forms);
+
+// let list = document.querySelector('.collection');
+
+// let listItems = Array.from(list.children)
+// for (let i = 0; i <= listItems.length; i++) {
+//   if (i % 2 === 0)
+//     listItems[i].innerHTML = 'odd'
+//   else
+//     listItems[i].innerHTML = 'even'
+// }
+// // listItems.forEach((item) => {
+//   console.log(item.innerText);
+// });
+
+const input = document.querySelector('#task');
+// input.addEventListener('paste', runEvent);
+
+// function runEvent(e) {
+//   e.preventDefault();
+//   console.log('You Can not paste here');
+// }

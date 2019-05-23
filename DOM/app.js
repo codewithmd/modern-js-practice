5afee0872c2c87c592a27dbf543cba591ecebb9f
codewithmmd <codewithmmd@gmail.com>
2019-05-23 20:20:02 +0530

    Play With Local Storage

--- DOM/app.js
+++ DOM/app.js
@@ -20,10 +20,41 @@
 //   console.log(item.innerText);
 // });
 
-const input = document.querySelector('#task');
+// const input = document.querySelector('#task');
+
 // input.addEventListener('paste', runEvent);
 
 // function runEvent(e) {
 //   e.preventDefault();
 //   console.log('You Can not paste here');
 // }
+
+document.querySelector('#task-form').addEventListener('submit', addTasks);
+document.querySelector('.clearBtn .btn').addEventListener('click', clearLocalStorage);
+
+function addTasks(e) {
+  e.preventDefault();
+
+  const value = document.querySelector('#task').value;
+  if (!value) return;
+
+  let tasks;
+  if (localStorage.getItem('tasks') === null)
+    tasks = [];
+  else
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  tasks.push(value);
+
+  tasks.forEach(element => {
+    console.log(element);
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
+}
+
+function clearLocalStorage(e) {
+  localStorage.clear();
+}
